9a0ba68a146a4b6f5fea6b363abdbf59b2087aef
luowenshuai <17865922869@163.com>
2021-02-25 16:37:41 +0800

    构建时添加hash

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,7 @@ export default defineConfig({
   title: 'components',
   favicon:
     'https://6865-heixongjun-ok4ws-1302448573.tcb.qcloud.la/xiongzhua.png',
-  logo:
-    'https://6865-heixongjun-ok4ws-1302448573.tcb.qcloud.la/xiong.png',
+  logo: 'https://6865-heixongjun-ok4ws-1302448573.tcb.qcloud.la/xiong.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
   extraBabelPlugins: [
@@ -18,4 +17,5 @@ export default defineConfig({
       },
     ],
   ],
+  hash: true,
 });
