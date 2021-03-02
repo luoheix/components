9b14ba30ac9d6a827986849b9a4374c80d89fd18
luowenshuai <17865922869@163.com>
2021-03-02 17:45:18 +0800

    添加 calenderManage 优化

--- src/CalendarManage/index.tsx
+++ src/CalendarManage/index.tsx
@@ -2,17 +2,23 @@ import React, { useState, useEffect } from 'react';
 import styles from './index.less';
 
 interface calendarChildListType {
-  dayNumber: number;
-  isTargetMonth: boolean;
-  isCurDay: boolean;
   weekNumber: number;
   weekName: string;
+  dateObj: Date;
 }
 
 interface calendarListType {
   key: string;
   children: calendarChildListType[];
 }
+
+// 当前时间
+const nowDate = new Date();
+const [nowYear, nowMonth, nowDay] = [
+  nowDate.getFullYear(),
+  nowDate.getMonth(),
+  nowDate.getDate(),
+];
 
 const weekMapList = [
   {
@@ -52,67 +58,22 @@ const switchDateObject = (date: Date | string) => {
     : new Date(date.replace(/-/g, '/'));
 };
 
-// 获取某月天数
-// date 格式 ‘Date 对象’ 或 ‘2020-9’
-const getMonthDays = (date = new Date()) => {
-  const targetDate = switchDateObject(date);
-  // 获取当前月份
-  const curMonth = targetDate.getMonth();
-  // 设置当前月1日，防止下一个月没有29-31日
-  targetDate.setDate(1);
-  // 将 targetDate 设置为下一个月
-  targetDate.setMonth(curMonth + 1);
-  // setDate(0) 会让 targetDate 日期变成前一个月的最后一天，也就是当前月的最后一天
-  targetDate.setDate(0);
-  // 获取 targetDate 的日期，也就是当前月的最后一天的日期，即当前月天数
-  return targetDate.getDate();
-};
-
-// 是否是当年当月当日
-const isCurYearMonthDay = (date: Date, dayNumber: number) => {
-  const targetDate = switchDateObject(date);
-  const nowDate = new Date();
-  return (
-    targetDate.getFullYear() === nowDate.getFullYear() &&
-    targetDate.getMonth() === nowDate.getMonth() &&
-    dayNumber === nowDate.getDate()
-  ); // 是否为当年当月
-};
-
 // 获取初始列表，6 横 7 列
 // date 格式 Date 对象 或 '2020-9'
-const getInitList = (date = new Date()): calendarListType[] => {
+const getInitList = (date = nowDate): calendarListType[] => {
   const targetDate = switchDateObject(date);
   targetDate.setDate(1); // 设置为当月第一天的星期数
   const targetWeekNumber = targetDate.getDay() || 7; // 目标月第一天
-  const targetMonthDays = getMonthDays(targetDate); // 目标月天数
-  targetDate.setDate(0); // 设置为上一月最后一天
-  const lastMonthDays = getMonthDays(targetDate); // 上个月天数
 
   const initList = [];
   for (let i = 0; i < 6; i += 1) {
     const listItem = weekMapList.map((item, index) => {
       let dayNumber = i * 7 + index - targetWeekNumber + 2; // 0
-      let isTargetMonth = false;
-      let isCurDay = false;
-      switch (true) {
-        case dayNumber < 1:
-          dayNumber += lastMonthDays;
-          break;
-        case dayNumber > targetMonthDays:
-          dayNumber -= targetMonthDays;
-          break;
-        default:
-          isTargetMonth = true;
-          isCurDay = isCurYearMonthDay(date, dayNumber);
-          break;
-      }
-
+      const targetDateCopy = switchDateObject(date);
+      targetDateCopy.setDate(dayNumber);
       return {
         ...item,
-        dayNumber,
-        isTargetMonth,
-        isCurDay,
+        dateObj: targetDateCopy,
       };
     });
     initList.push({
@@ -125,7 +86,7 @@ const getInitList = (date = new Date()): calendarListType[] => {
 };
 
 const CalendarManage: React.FC<{}> = () => {
-  const [targetDate, setTargetDate] = useState(new Date()); // switchDateObject('2020-10')
+  const [targetDate, setTargetDate] = useState(nowDate); // switchDateObject('2020-10')
   const [calendarList, setCalendarList] = useState<calendarListType[]>([]);
 
   useEffect(() => {
@@ -190,30 +151,62 @@ const CalendarManage: React.FC<{}> = () => {
         <div className={styles.body}>
           {calendarList.map(item => (
             <ul key={item.key}>
-              {item.children.map(it => (
-                <li
-                  style={
-                    it.isTargetMonth
-                      ? { color: 'rgba(0,0,0,.85)', backgroundColor: '#fafafa' }
-                      : {}
-                  }
-                  key={it.weekNumber}
-                >
-                  <div
-                    className={styles['day-number']}
+              {item.children.map(it => {
+                const { dateObj } = it;
+
+                // 日历项
+                const [itYear, itMonth, itDay] = [
+                  dateObj.getFullYear(),
+                  dateObj.getMonth(),
+                  dateObj.getDate(),
+                ];
+
+                // 目标月
+                const [targetYear, targetMonth] = [
+                  targetDate.getFullYear(),
+                  targetDate.getMonth(),
+                  targetDate.getDate(),
+                ];
+
+                // 是否是目标月
+                const isTargetMonth =
+                  itYear === targetYear && itMonth === targetMonth;
+
+                // 是否是今天
+                const isToday =
+                  itYear === nowYear &&
+                  itMonth === nowMonth &&
+                  itDay === nowDay;
+
+                return (
+                  <li
                     style={
-                      it.isCurDay
+                      isTargetMonth
                         ? {
-                            color: '#fff',
-                            background: 'rgb(0, 150, 250)',
+                            color: 'rgba(0,0,0,.85)',
+                            backgroundColor: '#fafafa',
                           }
                         : {}
                     }
+                    key={it.weekNumber}
                   >
-                    {it.dayNumber}
-                  </div>
-                </li>
-              ))}
+                    <div
+                      className={styles['day-number']}
+                      style={
+                        isToday
+                          ? {
+                              color: '#fff',
+                              background: 'rgb(0, 150, 250)',
+                            }
+                          : {}
+                      }
+                      title={`${itYear} 年 ${itMonth + 1} 月 ${itDay} 日`}
+                    >
+                      {itDay}
+                    </div>
+                  </li>
+                );
+              })}
             </ul>
           ))}
         </div>
